cb9fa119d0fa0eb9f00654e6ab795ad70959f459
goblinok <goblinok@gmail.com>
2018-08-30 01:35:09 +0900

    added favicon

--- public/js/app.js
+++ public/js/app.js
@@ -27,9 +27,11 @@ App = {
         // Is there an injected web3 instance?
         if (typeof web3 !== 'undefined') {
             App.web3Provider = web3.currentProvider;
+            console.log("web3 : (1) defined");
         } else {
             // If no injected web3 instance is detected, fall back to Ganache
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            console.log("web3 : (2) default");
         }
         web3 = new Web3(App.web3Provider);
         return App.initContract();
